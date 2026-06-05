6221f504f7088f86a9fb7ab682ece9a2e6287277
Zahradin Dib <zahradin.dib@gmail.com>
2026-06-05 14:57:53 +0100

    pthread_exit() if used in the main thread the prosses want terminate until threads end

--- src/main.c
+++ src/main.c
@@ -1,65 +1,44 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 #include <unistd.h>
 #include <pthread.h>
-#include <time.h>
-#include <errno.h>
 
 #define TH_NUM 10
 #define MU_NUM 4
 
-pthread_mutex_t stoveMutexes[MU_NUM];
-int stoveFuel[4] = {100, 100, 100, 100};
+int primes[10] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 };
 
 void* routine(void* arg) {
-    for (int i = 0; i <MU_NUM; i++) {
-        if (pthread_mutex_trylock(stoveMutexes + i) == 0) {
-            int fuelNeeded = (rand() % 30) + 1;
-            if (stoveFuel[i] < fuelNeeded) {
-                printf("no more fuel... time to go home\n");
-            }
-            else
-            {
-                stoveFuel[i] -= fuelNeeded;
-                usleep(500000);
-                printf("fuel left %d\n", stoveFuel[i]);
-            }
-            pthread_mutex_unlock(stoveMutexes + i);
-            break;
-        }
-        else
-        {
-            if (i == 3) {
-                printf("no stove available yet, waiting...\n");
-                usleep(300000);
-                i = 0;
-            }
-        }
+    int *sum = malloc(sizeof(int));
+    *sum = 0;
+    for (int i = 0; i < 5; i++) {
+        *sum += ((int*)arg)[i];
     }
+    printf("Thread sum : %d\n", *sum);
+    pthread_exit(sum);
 }
 
 int main(int argc, char *argv[]) {
-    srand(time(NULL));
-    pthread_t th[TH_NUM];
+    int sum = 0;
+    int th_num = 2;
+    pthread_t th[th_num];
 
-    for (int i = 0; i < MU_NUM; i++) {
-        pthread_mutex_init(stoveMutexes + i, NULL);
-    }
-    for (int i = 0; i < TH_NUM; i++) {
-        if (pthread_create(th + i, NULL, routine, NULL) != 0) {
+    for (int i = 0; i < th_num; i++) {
+        if (pthread_create(th + i, NULL, routine, primes+(5*i) ) != 0) {
             perror("failed to create thread!");
             return 1;
         }
     }
-    for (int i = 0; i < TH_NUM; i++) {
-        if (pthread_join(th[i], NULL) != 0) {
+    pthread_exit(0);
+
+    for (int i = 0; i < th_num; i++) {
+        int* resault;
+        if (pthread_join(th[i], (void**)&resault) != 0) {
             perror("failed to join thread!");
             return 2;
         }
+        sum += *resault;
     }
-    for (int i = 0; i < MU_NUM; i++) {
-        pthread_mutex_destroy(stoveMutexes + i);
-    }
-
-    return 0;
+    printf("the sum is %d\n", sum);
 }
