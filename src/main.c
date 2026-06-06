708b9a141e3f7282462c703bc3faa4b6141a034b
Zahradin Dib <zahradin.dib@gmail.com>
2026-06-06 20:01:48 +0100

    detached threads

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <bits/pthreadtypes.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -5,63 +6,28 @@
 #include <errno.h>
 #include <time.h>
 
-#define TH_NUM 8
+#define TH_NUM 2
 
-pthread_barrier_t barrierRolledDice;
-pthread_barrier_t barrierCalculated;
-int diceValues[8];
-int status[8] = {0};
-
-void* rollDice(void* arg) {
-    int index = *((int*)arg);
-    diceValues[index] = rand() % 6 + 1;
-    pthread_barrier_wait(&barrierRolledDice);
-    pthread_barrier_wait(&barrierCalculated);
-    if (status[index] == 1) {
-        printf("(%d rolled %d)I won\n", index, diceValues[index]);
-    }
-    else
-    {
-        printf("(%d rolled %d)I lost\n", index, diceValues[index]);
-    }
-    free(arg);
+void* routine(void* arg) {
+    sleep(1);
+    printf("finished execution.\n");
 }
 
 int main(int argc, char *argv[]) {
     pthread_t th[TH_NUM];
-    int max = 0;
+    pthread_attr_t detachedThread;
 
-    pthread_barrier_init(&barrierRolledDice, NULL, TH_NUM + 1);
-    pthread_barrier_init(&barrierCalculated, NULL, TH_NUM + 1);
+    pthread_attr_init(&detachedThread);
+    pthread_attr_setdetachstate(&detachedThread, PTHREAD_CREATE_DETACHED);
     for (int i = 0; i < TH_NUM; i++) {
-        int* a = malloc(sizeof(int));
-        *a = i;
-        if (pthread_create(th + i, NULL, rollDice, a) != 0) {
+        if (pthread_create(th + i, &detachedThread, routine, NULL) != 0) {
             perror("failed to create thread!");
-            return 1;
         }
+        // pthread_detach(th[i]); -> set the thread from to a detached state if it not
     }
-    pthread_barrier_wait(&barrierRolledDice);
-    for (int i = 0; i < TH_NUM; i++) {
-        if (diceValues[i] > max) max = diceValues[i];
-    }
-    for (int i = 0; i < TH_NUM; i++) {
-        if (diceValues[i] == max) {
-             status[i] = 1;
-        }
-        else {
-            status[i] = 0;
-        }
-    }
-    pthread_barrier_wait(&barrierCalculated);
-    for (int i = 0; i < TH_NUM; i++) {
-        if (pthread_join(th[i], NULL) != 0) {
-            perror("failed to join thread!");
-            return 2;
-        }
-    }
-    pthread_barrier_destroy(&barrierRolledDice);
-    pthread_barrier_destroy(&barrierCalculated);
+    // no need to wasting resources for waiting and joining threads
+    // pthread_join() is still necessary to get the return of the thread
+    pthread_attr_destroy(&detachedThread);
 
-    return 0;
+    pthread_exit(0);
 }
