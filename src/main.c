acbc3660a810606f6cc0ce29b2d6bea88fe86e45
Zahradin Dib <zahradin.dib@gmail.com>
2026-06-06 07:46:56 +0100

    pthread_barrier_wait()

--- src/main.c
+++ src/main.c
@@ -1,44 +1,67 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <time.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <errno.h>
+#include <time.h>
 
-#define TH_NUM 10
-#define MU_NUM 4
+#define TH_NUM 8
 
-int primes[10] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 };
+pthread_barrier_t barrierRolledDice;
+pthread_barrier_t barrierCalculated;
+int diceValues[8];
+int status[8] = {0};
 
-void* routine(void* arg) {
-    int *sum = malloc(sizeof(int));
-    *sum = 0;
-    for (int i = 0; i < 5; i++) {
-        *sum += ((int*)arg)[i];
+void* rollDice(void* arg) {
+    int index = *((int*)arg);
+    diceValues[index] = rand() % 6 + 1;
+    pthread_barrier_wait(&barrierRolledDice);
+    pthread_barrier_wait(&barrierCalculated);
+    if (status[index] == 1) {
+        printf("(%d rolled %d)I won\n", index, diceValues[index]);
     }
-    printf("Thread sum : %d\n", *sum);
-    pthread_exit(sum);
+    else
+    {
+        printf("(%d rolled %d)I lost\n", index, diceValues[index]);
+    }
+    free(arg);
 }
 
 int main(int argc, char *argv[]) {
-    int sum = 0;
-    int th_num = 2;
-    pthread_t th[th_num];
+    pthread_t th[TH_NUM];
+    int max = 0;
 
-    for (int i = 0; i < th_num; i++) {
-        if (pthread_create(th + i, NULL, routine, primes+(5*i) ) != 0) {
+    pthread_barrier_init(&barrierRolledDice, NULL, TH_NUM + 1);
+    pthread_barrier_init(&barrierCalculated, NULL, TH_NUM + 1);
+    for (int i = 0; i < TH_NUM; i++) {
+        int* a = malloc(sizeof(int));
+        *a = i;
+        if (pthread_create(th + i, NULL, rollDice, a) != 0) {
             perror("failed to create thread!");
             return 1;
         }
     }
-    pthread_exit(0);
-
-    for (int i = 0; i < th_num; i++) {
-        int* resault;
-        if (pthread_join(th[i], (void**)&resault) != 0) {
+    pthread_barrier_wait(&barrierRolledDice);
+    for (int i = 0; i < TH_NUM; i++) {
+        if (diceValues[i] > max) max = diceValues[i];
+    }
+    for (int i = 0; i < TH_NUM; i++) {
+        if (diceValues[i] == max) {
+             status[i] = 1;
+        }
+        else {
+            status[i] = 0;
+        }
+    }
+    pthread_barrier_wait(&barrierCalculated);
+    for (int i = 0; i < TH_NUM; i++) {
+        if (pthread_join(th[i], NULL) != 0) {
             perror("failed to join thread!");
             return 2;
         }
-        sum += *resault;
     }
-    printf("the sum is %d\n", sum);
+    pthread_barrier_destroy(&barrierRolledDice);
+    pthread_barrier_destroy(&barrierCalculated);
+
+    return 0;
 }
