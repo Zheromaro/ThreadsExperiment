96caa9774c45b421ba0086a7348e8b563c9ab391
Zahradin Dib <zahradin.dib@gmail.com>
2026-06-08 09:30:11 +0100

    binary semaphore

--- src/main.c
+++ src/main.c
@@ -6,70 +6,38 @@
 #include <time.h>
 #include <semaphore.h>
 
-#define TH_NUM 8
+#define TH_NUM 1
 
-pthread_mutex_t mutexBuffer;
-sem_t semEmpty;
-sem_t semFull;
-int buffer[10];
-int count = 0;
+pthread_mutex_t mutexFuel;
+sem_t semFuel;
+int *fuel;
 
-void* producer(void* arg) {
-    while (1) {
-        // Produce
-        int x = rand() % 100;
-
-        // Add to the buffer7
-        sem_wait(&semEmpty);
-        pthread_mutex_lock(&mutexBuffer);
-        buffer[count] = x;
-        count++;
-        pthread_mutex_unlock(&mutexBuffer);
-        sem_post(&semFull);
-    }
-}
-void* consumer(void* arg) {
-    while (1) {
-        // Remove from the buffer
-        sem_wait(&semFull);
-        pthread_mutex_lock(&mutexBuffer);
-        int y = buffer[count - 1];
-        count--;
-        pthread_mutex_unlock(&mutexBuffer);
-        sem_post(&semEmpty);
-
-        // Consume
-        printf("Got %d\n", y);
-        sleep(1);
-    }
+void* routine(void* arg) {
+    *fuel += 50;
+    printf("Current value is %d\n", *fuel);
+    sem_post(&semFuel);
 }
 
 int main(int argc, char *argv[]) {
-    srand(time(NULL));
     pthread_t th[TH_NUM];
+    fuel = malloc(sizeof(int));
+    *fuel = 50;
 
-    pthread_mutex_init(&mutexBuffer, NULL);
-    sem_init(&semEmpty, 0, 10);
-    sem_init(&semFull, 0, 0);
+    pthread_mutex_init(&mutexFuel, NULL);
+    sem_init(&semFuel, 0, 0);
     for (int i = 0; i < TH_NUM; i++) {
-        if (i % 2 == 0) {
-            if (pthread_create(th + i, NULL, &producer, NULL) != 0)
-                perror("failed to create thread!");
-        }
-        else
-        {
-            if (pthread_create(th + i, NULL, &consumer, NULL) != 0)
-                perror("failed to create thread!");
-        }
+        if (pthread_create(th + i, NULL, &routine, NULL) != 0)
+            perror("failed to create thread!");
     }
+    sem_wait(&semFuel);
+    free(fuel);
+    printf("Deallocated fuel.\n");
     for (int i = 0; i < TH_NUM; i++) {
-        if (pthread_join(th[i], NULL) != 0) {
+        if (pthread_join(th[i], NULL) != 0)
             perror("failed to join thread!");
-        }
     }
-    sem_destroy(&semEmpty);
-    sem_destroy(&semFull);
-    pthread_mutex_destroy(&mutexBuffer);
+    pthread_mutex_destroy(&mutexFuel);
+    sem_destroy(&semFuel);
 
     return 0;
 }
