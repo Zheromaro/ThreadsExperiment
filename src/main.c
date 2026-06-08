ad6b31a2517c7856b1d1728c9e63fd02e1b12eea
Zahradin Dib <zahradin.dib@gmail.com>
2026-06-08 19:49:27 +0100

    sem_getvalue()

--- src/main.c
+++ src/main.c
@@ -1,43 +1,47 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <strings.h>
 #include <unistd.h>
 #include <errno.h>
 #include <time.h>
 #include <semaphore.h>
 
-#define TH_NUM 1
+#define TH_NUM 4
 
 pthread_mutex_t mutexFuel;
-sem_t semFuel;
-int *fuel;
+sem_t sem;
 
 void* routine(void* arg) {
-    *fuel += 50;
-    printf("Current value is %d\n", *fuel);
-    sem_post(&semFuel);
+    int index = *((int*)arg);
+    int semVal;
+    sem_wait(&sem);
+    sleep(index+1);
+    sem_getvalue(&sem, &semVal);
+    printf("(%d) Current semaphore value after wait is %d\n", index, semVal);
+    sem_post(&sem);
+    sem_getvalue(&sem, &semVal);
+    printf("(%d) Current semaphore value after post is %d\n", index, semVal);
+    free(arg);
 }
 
 int main(int argc, char *argv[]) {
     pthread_t th[TH_NUM];
-    fuel = malloc(sizeof(int));
-    *fuel = 50;
 
     pthread_mutex_init(&mutexFuel, NULL);
-    sem_init(&semFuel, 0, 0);
+    sem_init(&sem, 0, 4);
     for (int i = 0; i < TH_NUM; i++) {
-        if (pthread_create(th + i, NULL, &routine, NULL) != 0)
+        int* a = malloc(sizeof(int));
+        *a = i;
+        if (pthread_create(th + i, NULL, &routine, a) != 0)
             perror("failed to create thread!");
     }
-    sem_wait(&semFuel);
-    free(fuel);
-    printf("Deallocated fuel.\n");
     for (int i = 0; i < TH_NUM; i++) {
         if (pthread_join(th[i], NULL) != 0)
             perror("failed to join thread!");
     }
     pthread_mutex_destroy(&mutexFuel);
-    sem_destroy(&semFuel);
+    sem_destroy(&sem);
 
     return 0;
 }
