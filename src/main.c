f55053f94b20c0ded97744c46498ce2c43697790
Zahradin Dib <zahradin.dib@gmail.com>
2026-06-09 11:03:20 +0100

    Thread pools

--- src/main.c
+++ src/main.c
@@ -2,46 +2,92 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <strings.h>
-#include <unistd.h>
-#include <errno.h>
 #include <time.h>
+#include <unistd.h>
 #include <semaphore.h>
 
 #define TH_NUM 4
 
-pthread_mutex_t mutexFuel;
-sem_t sem;
+typedef struct Task {
+    void (*taskFunction)(int, int);
+    int arg1, arg2;
+} Task;
+Task taskQueue[256];
+int taskCount = 0;
 
-void* routine(void* arg) {
-    int index = *((int*)arg);
-    int semVal;
-    sem_wait(&sem);
-    sleep(index+1);
-    sem_getvalue(&sem, &semVal);
-    printf("(%d) Current semaphore value after wait is %d\n", index, semVal);
-    sem_post(&sem);
-    sem_getvalue(&sem, &semVal);
-    printf("(%d) Current semaphore value after post is %d\n", index, semVal);
-    free(arg);
+pthread_mutex_t mutexQueue;
+pthread_cond_t condQueue;
+
+void sum(int a, int b) {
+    usleep(50000);
+    int sum = a + b;
+    printf("the sum of %d and %d is %d\n", a, b, sum);
 }
+void product(int a, int b) {
+    usleep(50000);
+    int prod = a + b;
+    printf("the product of %d and %d is %d\n", a, b, prod);
+}
+
+void executeTask(Task* task) {
+    task->taskFunction(task->arg1, task->arg2);
+}
+
+void submitTask(Task task) {
+    pthread_mutex_lock(&mutexQueue);
+    taskQueue[taskCount] = task;
+    taskCount++;
+    pthread_mutex_unlock(&mutexQueue);
+    pthread_cond_signal(&condQueue);
+}
+
+void* startTread(void *arg) {
+    Task task;
+    int found = 0;
+    while (1) {
+        pthread_mutex_lock(&mutexQueue);
+        while (taskCount == 0) {
+            pthread_cond_wait(&condQueue, &mutexQueue);
+        }
+
+        task = taskQueue[0];
+        for (int i = 0; i < taskCount-1; i++)
+            taskQueue[i] = taskQueue[i+1];
+        taskCount--;
+        pthread_mutex_unlock(&mutexQueue);
+        executeTask(&task);
+    }
+}
+
 
 int main(int argc, char *argv[]) {
     pthread_t th[TH_NUM];
 
-    pthread_mutex_init(&mutexFuel, NULL);
-    sem_init(&sem, 0, 4);
+    pthread_mutex_init(&mutexQueue, NULL);
+    pthread_cond_init(&condQueue, NULL);
     for (int i = 0; i < TH_NUM; i++) {
         int* a = malloc(sizeof(int));
         *a = i;
-        if (pthread_create(th + i, NULL, &routine, a) != 0)
+        if (pthread_create(th + i, NULL, &startTread, a) != 0)
             perror("failed to create thread!");
     }
+
+    srand(time(NULL));
+    for (int i = 0; i < 100; i++) {
+        Task t = {
+            .taskFunction = (i % 2 == 0) ? &sum : &product,
+            .arg1 = rand() % 100,
+            .arg2 = rand() % 100
+        };
+        submitTask(t);
+    }
+
     for (int i = 0; i < TH_NUM; i++) {
         if (pthread_join(th[i], NULL) != 0)
             perror("failed to join thread!");
     }
-    pthread_mutex_destroy(&mutexFuel);
-    sem_destroy(&sem);
+    pthread_mutex_destroy(&mutexQueue);
+    pthread_cond_destroy(&condQueue);
 
     return 0;
 }
